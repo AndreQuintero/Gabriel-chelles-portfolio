9ba04ef9ba1e0c1ec289d351ba5eda0cb4182e90
Andre Quintero dos Santos <andrequintero@Andres-MacBook-Air.local>
2023-11-28 19:27:14 +0800

    feature wrapper implementation, providing theme to the application

---
+++ src/app/components/wrapper/wrapper.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { ReactNode, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme, Theme } from '@/app/style/theme'
+type Props = {
+  children: ReactNode
+}
+
+export const Wrapper = ({ children }: Props) => {
+  const [themeType, setThemeType] = useState<Theme>(Theme.LIGHT)
+  const toggleTheme = () =>
+    themeType === Theme.LIGHT
+      ? setThemeType(Theme.DARK)
+      : setThemeType(Theme.LIGHT)
+  const theme = () => (themeType === Theme.LIGHT ? lightTheme : darkTheme)
+  return (
+    <ThemeProvider theme={theme()}>
+      <button onClick={toggleTheme}>change theme</button>
+      {children}
+    </ThemeProvider>
+  )
+}
