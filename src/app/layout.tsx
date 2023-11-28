38eadb75a279be428681c94302731aaf0e6132fe
Andre Quintero dos Santos <andrequintero@Andres-MacBook-Air.local>
2023-11-28 14:16:12 +0800

    config: set up styled-components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins, Outfit } from 'next/font/google'
-import './globals.css'
+
+import StyledComponentsRegistry from './lib/registry'
 
 export const poppins = Poppins({
   subsets: ['latin'],
@@ -26,7 +27,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={outfit.className}>{children}</body>
+      <body className={outfit.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
