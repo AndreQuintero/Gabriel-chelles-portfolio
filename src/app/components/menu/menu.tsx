d1ae94b01bbefd6eee97d5477c9b11375e57c78c
Andre Quintero dos Santos <andrequintero@Andres-MacBook-Air.local>
2023-11-30 21:07:19 +0800

    add: menu mobile implementation, and animation

--- src/app/components/menu/menu.tsx
+++ src/app/components/menu/menu.tsx
@@ -1,23 +1,51 @@
 'use client'
+import { useTheme } from 'styled-components'
+import { useState } from 'react'
+import { AnimatePresence, delay, motion } from 'framer-motion'
+
 import { useViewDetect } from '@/app/hooks/useViewDetect'
 import * as S from './style'
 
+import { Navigation } from './navigation'
+import { MenuIconOpened, MenuItemClosed } from './menu-icon'
+
 const Menu = () => {
   const { isMobile } = useViewDetect()
+  const { colors } = useTheme()
+  const [isMenuVisible, setIsMenuVisible] = useState(false)
+
+  const toggleMenu = () => setIsMenuVisible(!isMenuVisible)
+
   return (
-    <S.Header>
-      <S.PortfolioName>Gabriel Chelles</S.PortfolioName>
-      <nav>
-        <S.LinksList>
-          <li>
-            <S.Link href="#">Projetos</S.Link>
-          </li>
-          <li>
-            <S.Link href="#">Sobre</S.Link>
-          </li>
-        </S.LinksList>
-      </nav>
-    </S.Header>
+    <>
+      <S.Header>
+        <S.PortfolioName>Gabriel Chelles</S.PortfolioName>
+        {!isMobile && <Navigation isMobile={isMobile} />}
+        {isMobile && (
+          <div onClick={toggleMenu}>
+            {!isMenuVisible ? (
+              <MenuIconOpened color={colors.secondary} />
+            ) : (
+              <MenuItemClosed color={colors.secondary} />
+            )}
+          </div>
+        )}
+      </S.Header>
+      <AnimatePresence>{isMenuVisible && <MenuMobile />}</AnimatePresence>
+    </>
+  )
+}
+
+const MenuMobile = () => {
+  return (
+    <S.MenuMobile
+      key="menu"
+      initial={{ opacity: 0, y: -200 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2, ease: 'easeIn' }}
+      exit={{ opacity: 0, y: -200 }}>
+      <Navigation isMobile />
+    </S.MenuMobile>
   )
 }
 
