54314639f82e37468efd2ef5385311d0e3d2d819
AndreQuintero <andre.quintero96@gmail.com>
2023-12-01 00:08:40 +0800

    feature: first implementation of hero section with apresentation texts

--- src/app/templates/home/hero/hero.tsx
+++ src/app/templates/home/hero/hero.tsx
@@ -1,3 +1,20 @@
+'use client'
+import { Container } from '@/app/components/container'
+import * as S from './style'
+
 export const Hero = () => {
-  return <section>Hero</section>
+  return (
+    <S.HeroSection>
+      <Container>
+        <S.Apresentation>
+          Hey! sou <S.Name> Gabriel Chelles</S.Name>
+        </S.Apresentation>
+        <S.Description>
+          Product Designer atualmente ajudando a{' '}
+          <S.Highlight> Gamers Club </S.Highlight> a se tornar a maior empresa
+          de esports do mundo!🤘
+        </S.Description>
+      </Container>
+    </S.HeroSection>
+  )
 }
