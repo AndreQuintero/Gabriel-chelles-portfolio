fab9a8df1ec61be0594dc2f066fd254e59408b28
Andre Quintero dos Santos <andrequintero@Andres-MacBook-Air.local>
2023-11-30 22:34:55 +0800

    delete: component test deleted, it was temporaly

--- src/app/components/test.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-import styled, { css } from 'styled-components'
-
-export const Text = styled.h1`
-  ${({ theme }) => css`
-    font-size: 3.4rem;
-    color: ${theme.colors.secondary};
-  `}
-`
