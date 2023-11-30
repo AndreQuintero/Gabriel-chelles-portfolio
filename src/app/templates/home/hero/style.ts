a704bb251e7166f3bd14730604857f98e431c47e
AndreQuintero <andre.quintero96@gmail.com>
2023-12-01 00:09:29 +0800

    style: text styles implementation

--- src/app/templates/home/hero/style.ts
+++ src/app/templates/home/hero/style.ts
@@ -1,1 +1,41 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const HeroSection = styled.section`
+  margin-top: 6.4rem;
+  ${media.greaterThan('medium')`
+        margin-top: 19.6rem;
+    `}
+`
+
+export const Apresentation = styled.h2`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.style.primary};
+    font-size: 1.8rem;
+    font-weight: 400;
+    ${media.greaterThan('medium')`
+        font-size: 4.8rem;
+    `}
+  `}
+`
+export const Name = styled.span`
+  font-weight: 600;
+`
+
+export const Description = styled.h1`
+  ${({ theme }) => css`
+    margin-top: 2.4rem;
+    font-family: ${theme.fonts.style.primary};
+    font-size: 2.8rem;
+    font-style: normal;
+    font-weight: 400;
+    ${media.greaterThan('medium')`
+        font-size: 7.2rem;
+        margin-top: 4rem
+    `}
+  `}
+`
+
+export const Highlight = styled.span`
+  color: #1a70d0;
+`
