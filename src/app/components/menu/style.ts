dccb94bdf05aed829ab58e7753912ed5eeb5f285
Andre Quintero dos Santos <andrequintero@Andres-MacBook-Air.local>
2023-11-30 21:07:49 +0800

    style: menu mobile appereance

--- src/app/components/menu/style.ts
+++ src/app/components/menu/style.ts
@@ -1,52 +1,36 @@
+import { motion } from 'framer-motion'
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Header = styled.header`
   display: flex;
   width: 100%;
-  padding: 2.4rem 4.8rem;
+  padding: 1.6rem;
   justify-content: space-between;
   align-items: center;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  ${media.greaterThan('medium')`
+    padding: 2.4rem 4.8rem;
+  `}
 `
 
 export const PortfolioName = styled.label`
   ${({ theme }) => css`
     font-family: ${theme.fonts.style.secondary};
     color: ${theme.colors.secondary};
-    font-size: 24px;
+    font-size: 2.4rem;
     font-weight: 400;
   `}
 `
 
-export const LinksList = styled.ul`
-  list-style-type: none;
-  display: flex;
-  gap: 6.4rem;
-`
-export const Link = styled.a`
+export const MenuMobile = styled(motion.menu)`
   ${({ theme }) => css`
-    font-family: ${theme.fonts.style.secondary};
-    font-size: 1.8rem;
-    font-style: normal;
-    font-weight: 400;
-    text-decoration: none;
-    color: ${theme.colors.secondary};
-    position: relative;
-
-    &::after {
-      content: '';
-      display: block;
-      width: 0;
-      position: absolute;
-      background: transparent;
-      transition: all 0.3s;
-      height: 2px;
-      left: 0;
-      right: 0;
-      margin: 2.5rem auto;
-    }
-    &:hover::after {
-      width: 100%;
-      background: ${theme.colors.secondary};
-    }
+    width: 100%;
+    height: calc(100vh - 4.8rem);
+    background-color: ${theme.colors.primary};
+    position: fixed;
+    z-index: 9;
   `}
 `
