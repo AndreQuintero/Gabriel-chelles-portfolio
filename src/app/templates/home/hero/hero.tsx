29e3cf5a895e388a4be27264bedefba4e07c1c88
AndreQuintero <andre.quintero96@gmail.com>
2023-12-01 17:50:47 +0800

    change: added viewport property for not to repeat the animations again

--- src/app/templates/home/hero/hero.tsx
+++ src/app/templates/home/hero/hero.tsx
@@ -11,18 +11,24 @@ export const Hero = () => {
         <S.Apresentation
           variants={fadeIn(0.2)}
           initial="hidden"
-          whileInView="show">
+          whileInView="show"
+          viewport={{ once: true }}>
           Hey! sou <S.Name> Gabriel Chelles</S.Name>
         </S.Apresentation>
         <S.Description
           variants={fadeIn(0.4)}
           initial="hidden"
-          whileInView="show">
+          whileInView="show"
+          viewport={{ once: true }}>
           Product Designer atualmente ajudando a{' '}
           <S.Highlight> Gamers Club </S.Highlight> a se tornar a maior empresa
           de esports do mundo!🤘
         </S.Description>
-        <S.Buttons variants={fadeIn(0.6)} initial="hidden" whileInView="show">
+        <S.Buttons
+          variants={fadeIn(0.6)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}>
           <Button type="primary" icon="linkedin">
             /gabrielChelles
           </Button>
