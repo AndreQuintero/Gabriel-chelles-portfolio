a44a5b6ce9100e9651e07c86ef97ebc2520d0250
AndreQuintero <andre.quintero96@gmail.com>
2023-12-01 17:52:05 +0800

    style: video section implementation

--- src/app/templates/home/video/style.ts
+++ src/app/templates/home/video/style.ts
@@ -1,1 +1,21 @@
+import { motion } from 'framer-motion'
 import styled from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled(motion.section)`
+  margin-top: 6.4rem;
+  display: flex;
+  justify-content: center;
+  ${media.greaterThan('medium')`
+        margin-top: 19.2rem;
+    `}
+
+  iframe {
+    width: 100%;
+    height: 46.4rem;
+
+    ${media.greaterThan('medium')`
+        height: 72rem;
+    `}
+  }
+`
