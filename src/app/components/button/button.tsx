0e7379a3ca48005b56b99dc718ada79701181a72
AndreQuintero <andre.quintero96@gmail.com>
2023-12-01 17:03:06 +0800

    feature: button component implementation

---
+++ src/app/components/button/button.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react'
+import * as S from './style'
+
+import { DownloadIcon, LinkedinIcon } from '@/app/assets/icons'
+
+type ButtonProps = {
+  children: ReactNode
+  type: 'primary' | 'secondary'
+  icon?: 'linkedin' | 'download'
+}
+
+const Icons = {
+  linkedin: <LinkedinIcon />,
+  download: <DownloadIcon />
+}
+
+export const Button = ({ children, type, icon }: ButtonProps) => {
+  return (
+    <S.Button type={type}>
+      {!!icon && Icons[icon]} {children}
+    </S.Button>
+  )
+}
+
+type IconProps = { color: string }
