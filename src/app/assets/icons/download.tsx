a92e27d0cbd7e30d69f9bf42d39edb1cb43fe87b
AndreQuintero <andre.quintero96@gmail.com>
2023-12-01 17:01:20 +0800

    add: svg icons for butttons

---
+++ src/app/assets/icons/download.tsx
@@ -0,0 +1,17 @@
+export const DownloadIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12.5303 16.2803L12 16.8107L11.4697 16.2803L5.46967 10.2803L6.53033 9.21967L11.25 13.9393V2.25H12.75V13.9393L17.4697 9.21967L18.5303 10.2803L12.5303 16.2803ZM2.25 15.75V20.25V21H3H21H21.75V20.25V15.75H20.25V19.5H3.75V15.75H2.25Z"
+        fill="none"
+      />
+    </svg>
+  )
+}
