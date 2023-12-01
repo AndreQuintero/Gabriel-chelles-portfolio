e02e61e6307ccc4a5a96c33c805643fd09ad0ef3
AndreQuintero <andre.quintero96@gmail.com>
2023-12-01 16:59:30 +0800

    add: implementing buttons section and animation fade in

--- src/app/templates/home/hero/hero.tsx
+++ src/app/templates/home/hero/hero.tsx
@@ -1,19 +1,35 @@
 'use client'
 import { Container } from '@/app/components/container'
 import * as S from './style'
+import { Button } from '@/app/components/button'
+import { fadeIn } from '@/app/motion'
 
 export const Hero = () => {
   return (
     <S.HeroSection>
       <Container>
-        <S.Apresentation>
+        <S.Apresentation
+          variants={fadeIn(0.2)}
+          initial="hidden"
+          whileInView="show">
           Hey! sou <S.Name> Gabriel Chelles</S.Name>
         </S.Apresentation>
-        <S.Description>
+        <S.Description
+          variants={fadeIn(0.4)}
+          initial="hidden"
+          whileInView="show">
           Product Designer atualmente ajudando a{' '}
           <S.Highlight> Gamers Club </S.Highlight> a se tornar a maior empresa
           de esports do mundo!🤘
         </S.Description>
+        <S.Buttons variants={fadeIn(0.6)} initial="hidden" whileInView="show">
+          <Button type="primary" icon="linkedin">
+            /gabrielChelles
+          </Button>
+          <Button type="primary" icon="download">
+            Download CV
+          </Button>
+        </S.Buttons>
       </Container>
     </S.HeroSection>
   )
