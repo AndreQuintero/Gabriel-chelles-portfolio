1b6a743b7879825383faf9dfa5d9c230cf1b4119
AndreQuintero <andre.quintero96@gmail.com>
2023-12-02 22:08:27 +0800

    style: custom cursor style implementation

---
+++ src/app/components/custom-cursor/style.ts
@@ -0,0 +1,37 @@
+import styled, { css } from 'styled-components'
+
+export const Cursor = styled.div`
+  ${({ theme }) => css`
+    width: 16rem;
+    height: 16rem;
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    position: absolute;
+    pointer-events: none;
+
+    label {
+      color: #252525;
+      font-family: ${theme.fonts.style.secondary};
+      font-size: 1.8rem;
+      font-weight: 400;
+    }
+  `}
+`
+
+export const Wrapper = styled.div`
+  display: contents;
+  position: relative;
+  cursor: none;
+  ${Cursor} {
+    opacity: 0;
+  }
+
+  &:hover {
+    ${Cursor} {
+      opacity: 1;
+    }
+  }
+`
