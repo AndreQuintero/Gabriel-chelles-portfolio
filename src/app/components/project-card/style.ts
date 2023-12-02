a875b6d197f64e5c6a7487c2f13659e2b7642bb3
AndreQuintero <andre.quintero96@gmail.com>
2023-12-02 22:10:43 +0800

    style: initial project card implementation

---
+++ src/app/components/project-card/style.ts
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 58.6rem;
+  height: 63.2rem;
+`
+
+export const ProjectImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+`
