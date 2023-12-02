39e92f897c6ed5aba19213d420f79a9893a65938
AndreQuintero <andre.quintero96@gmail.com>
2023-12-02 22:09:23 +0800

    fix: fixing fill rules and clip rules to react pattern

--- src/app/components/menu/menu-icon/menu-icon.tsx
+++ src/app/components/menu/menu-icon/menu-icon.tsx
@@ -13,8 +13,8 @@ export const MenuIconOpened = ({ color }: MenuIconProps) => {
         viewBox="0 0 32 32"
         fill="none">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M29 9H3V7H29V9ZM29 17H3V15H29V17ZM3 25H29V23H3V25Z"
           fill={color}
         />
@@ -33,8 +33,8 @@ export const MenuItemClosed = ({ color }: MenuIconProps) => {
         viewBox="0 0 32 32"
         fill="none">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M16 14.5859L25.2928 5.29297L26.7071 6.70718L17.4142 16.0001L26.7071 25.293L25.2928 26.7072L16 17.4143L6.70706 26.7072L5.29285 25.293L14.5857 16.0001L5.29285 6.70718L6.70706 5.29297L16 14.5859Z"
           fill={color}
         />
