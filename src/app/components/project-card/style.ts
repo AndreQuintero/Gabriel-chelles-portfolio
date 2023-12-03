bf9ae1f26d572f3ea8d854350e6598a7738d3294
AndreQuintero <andre.quintero96@gmail.com>
2023-12-04 01:25:42 +0800

    style: project card style implementation

---
+++ src/app/components/project-card/style.ts
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Title = styled.h4`
+  ${({ theme }) => css`
+    margin-top: 1.6rem;
+    font-family: ${theme.fonts.style.primary};
+    font-size: 2.4rem;
+    color: ${theme.colors.secondary};
+    font-weight: 400;
+
+    ${media.greaterThan('medium')`
+      margin-top: 3.2rem;
+      font-size: 4rem;
+    `}
+  `}
+`
+
+export const Wrapper = styled.a`
+  text-decoration: none;
+  &:hover h4 {
+    text-decoration: underline;
+  }
+
+  ${media.greaterThan('medium')`
+    display: table-caption;
+  `}
+`
