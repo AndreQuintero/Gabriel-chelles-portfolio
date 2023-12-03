62e8a19a22310d62a818f76a13f80cd66f6b79e8
AndreQuintero <andre.quintero96@gmail.com>
2023-12-03 19:43:27 +0800

    add: button native props added to the component

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -1,23 +1,28 @@
 'use client'
-import { ReactNode } from 'react'
+import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react'
 import * as S from './style'
 
 import { DownloadIcon, LinkedinIcon } from '@/app/assets/icons'
 
 type ButtonProps = {
   children: ReactNode
-  type: 'primary' | 'secondary'
+  colorScheme: 'primary' | 'secondary'
   icon?: 'linkedin' | 'download'
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
 const Icons = {
   linkedin: <LinkedinIcon />,
   download: <DownloadIcon />
 }
 
-export const Button = ({ children, type, icon }: ButtonProps) => {
+export const Button = ({
+  children,
+  colorScheme,
+  icon,
+  ...props
+}: ButtonProps) => {
   return (
-    <S.Button type={type}>
+    <S.Button $colorScheme={colorScheme} {...props}>
       {!!icon && Icons[icon]} {children}
     </S.Button>
   )
