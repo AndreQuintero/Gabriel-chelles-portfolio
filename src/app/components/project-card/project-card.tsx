9bc3b3e4a2a49810a6933c6c513277eaaad2da9d
AndreQuintero <andre.quintero96@gmail.com>
2023-12-03 19:48:37 +0800

    feature: project card component implementation

--- src/app/components/project-card/project-card.tsx
+++ src/app/components/project-card/project-card.tsx
@@ -1,13 +1,64 @@
 'use client'
-import Thumbnail from '@/app/assets/projects/thumbnail.png'
 
 import * as S from './style'
 import { CustomCursor, WithCustomCursor } from '../custom-cursor'
+import { useRef, useState } from 'react'
 
-const ProjectCard = ({ onMouseMove }: CustomCursor) => {
+type ImageProps = {
+  url: string
+  description: string
+}
+
+type ProjectCardProps = {
+  images: Array<ImageProps>
+} & CustomCursor
+
+const ProjectCard = ({ onMouseMove, images }: ProjectCardProps) => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
+  const SCREEN_TIME = 1000
+
+  const handleImagesWhenHoverit = () => {
+    intervalRef.current = setInterval(() => {
+      setActiveIndex((previous) =>
+        previous === images.length - 1 ? 0 : previous + 1
+      )
+    }, SCREEN_TIME)
+  }
+
+  const handleImagesWhenLeaveIt = () => {
+    setActiveIndex(0)
+    clearInterval(intervalRef.current as NodeJS.Timeout)
+  }
   return (
-    <S.Wrapper onMouseMove={onMouseMove}>
-      <S.ProjectImage src={Thumbnail} alt="Projeto 1" />
+    <S.Wrapper
+      onMouseMove={onMouseMove}
+      onMouseEnter={handleImagesWhenHoverit}
+      onMouseLeave={handleImagesWhenLeaveIt}>
+      {images.map((img, index) => {
+        const shoudDisplayIt = () => (activeIndex === index ? 1 : 0)
+        return (
+          <S.ImgWrapper
+            key={img.url}
+            initial={{
+              opacity: shoudDisplayIt()
+            }}
+            animate={{
+              opacity: shoudDisplayIt()
+            }}
+            transition={{
+              delay: 0.15,
+              ease: 'easeIn'
+            }}>
+            <S.ProjectImage
+              fill
+              style={{ objectFit: 'cover' }}
+              src={img.url}
+              alt={img.description}
+            />
+          </S.ImgWrapper>
+        )
+      })}
     </S.Wrapper>
   )
 }
