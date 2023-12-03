f4ae787f11ba2f1585a58d6156830529434f4097
AndreQuintero <andre.quintero96@gmail.com>
2023-12-03 19:49:38 +0800

    style:  project card style implementation

--- src/app/components/project-card/style.ts
+++ src/app/components/project-card/style.ts
@@ -1,12 +1,25 @@
+import { motion } from 'framer-motion'
 import Image from 'next/image'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
-  width: 58.6rem;
+  width: 100%;
   height: 63.2rem;
+  position: relative;
+  ${media.greaterThan('medium')`
+      width: 58.6rem;
+  `}
 `
 
 export const ProjectImage = styled(Image)`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  transition: 150ms ease-in;
+`
+export const ImgWrapper = styled(motion.div)`
+  position: absolute;
   width: 100%;
   height: 100%;
 `
