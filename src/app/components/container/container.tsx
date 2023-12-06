cc710f190339e7b49b0e9a48aa6725bfe6f526aa
AndreQuintero <andre.quintero96@gmail.com>
2023-12-07 02:05:20 +0800

    add: ignorerighpadding prop implementation

--- src/app/components/container/container.tsx
+++ src/app/components/container/container.tsx
@@ -5,12 +5,16 @@ import * as S from './style'
 type ContainerProps = {
   children: ReactNode
   ignorePaddingOnMobile?: boolean
+  ignoreRightPadding?: boolean
 }
 export const Container = ({
   children,
-  ignorePaddingOnMobile = false
+  ignorePaddingOnMobile = false,
+  ignoreRightPadding = false
 }: ContainerProps) => (
-  <S.Container $ignorePaddingOnMobile={ignorePaddingOnMobile}>
+  <S.Container
+    $ignorePaddingOnMobile={ignorePaddingOnMobile}
+    $ignoreRightPadding={ignoreRightPadding}>
     {children}
   </S.Container>
 )
--- src/app/components/container/style.ts
+++ src/app/components/container/style.ts
@@ -1,14 +1,17 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
-export const Container = styled.div<{ $ignorePaddingOnMobile: boolean }>`
-  ${({ $ignorePaddingOnMobile }) => css`
+export const Container = styled.div<{
+  $ignorePaddingOnMobile: boolean
+  $ignoreRightPadding: boolean
+}>`
+  ${({ $ignorePaddingOnMobile, $ignoreRightPadding }) => css`
     max-width: 1314px;
     margin-left: auto;
     margin-right: auto;
-
     ${media.lessThan('large')`
-      padding: 0 ${$ignorePaddingOnMobile ? '0' : '1.6rem'}
+      padding: 0 ${$ignorePaddingOnMobile ? '0' : '1.6rem'};
+      ${$ignoreRightPadding && 'padding-right: 0;'}
     `}
   `}
 `
