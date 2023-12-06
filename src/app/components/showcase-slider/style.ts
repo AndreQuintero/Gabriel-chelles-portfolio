111d40ba711a154bc358bcb838d6fc44d268cb4f
AndreQuintero <andre.quintero96@gmail.com>
2023-12-07 02:07:14 +0800

    style: showcase slider style implementation

---
+++ src/app/components/showcase-slider/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const ShowcaseSlider = styled.div`
+  overflow: hidden;
+  margin-top: 1.6rem;
+  .slick-track {
+    display: flex;
+    margin: -10px -10px; /* Adjust the negative margin to control the gap size for rows and columns */
+  }
+
+  .slick-slide {
+    margin: 10px 10px; /* Adjust the margin to match the negative margin above */
+  }
+`
