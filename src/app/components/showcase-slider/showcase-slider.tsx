9a27f47ef9af50d42dcca5adbe9fafe1b663cd2d
AndreQuintero <andre.quintero96@gmail.com>
2023-12-07 03:10:03 +0800

    change: removing unused import

--- src/app/components/showcase-slider/showcase-slider.tsx
+++ src/app/components/showcase-slider/showcase-slider.tsx
@@ -1,10 +1,11 @@
 import Slider from 'react-slick'
 
 import * as S from './style'
+import { CompanyCard } from '../company-card'
+import { ShowcaseProps } from '../showcase'
+
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import { CompanyCard, CompanyCardProps } from '../company-card'
-import { ShowcaseProps } from '../showcase'
 
 export const ShowCaseSlider = ({ companies }: ShowcaseProps) => {
   const settings = {
