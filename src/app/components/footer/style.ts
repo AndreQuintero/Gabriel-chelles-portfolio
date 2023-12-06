2f4c8a8f87dac89236e803fb68dbf948ba0850af
AndreQuintero <andre.quintero96@gmail.com>
2023-12-07 02:54:26 +0800

    style: footer style implementation

--- src/app/components/footer/style.ts
+++ src/app/components/footer/style.ts
@@ -1,1 +1,48 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Footer = styled.footer`
+  ${({ theme }) => css`
+    padding-top: 6.4rem;
+    padding-bottom: 6.4rem;
+    background-color: ${theme.colors.tertiary};
+    transition: all 0.2s ease-in;
+
+    ${media.greaterThan('medium')`
+    padding-top: 12.8rem;
+    padding-bottom: 14.2rem;
+    `}
+  `}
+`
+
+export const Title = styled.h1`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.style.primary};
+    font-size: 2.4rem;
+    font-weight: 400;
+
+    ${media.greaterThan('medium')`
+        font-size: 6.4rem;
+    `}
+  `}
+`
+
+export const Showcase = styled.h4`
+  ${({ theme }) => css`
+    margin-top: 6.4rem;
+    font-family: ${theme.fonts.style.secondary};
+    font-size: 1.4rem;
+    font-weight: 400;
+    ${media.greaterThan('medium')`
+        margin-top: 8.8rem;
+        font-size: 2.4rem;
+    `}
+  `}
+`
+
+export const ContactWrapper = styled.div`
+  margin-top: 6.4rem;
+  ${media.greaterThan('medium')`
+    margin-top: 18.3rem;
+  `}
+`
