78b466b70afaff5ee470eb13e1ce69a65ad58087
AndreQuintero <andre.quintero96@gmail.com>
2023-12-07 22:06:38 +0800

    change: hidden custom cursor on mobile

--- src/app/components/custom-cursor/style.ts
+++ src/app/components/custom-cursor/style.ts
@@ -1,17 +1,18 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Cursor = styled.div`
   ${({ theme }) => css`
     width: 16rem;
     height: 16rem;
     background-color: #fff;
-    display: flex;
     align-items: center;
     justify-content: center;
     border-radius: 50%;
     position: absolute;
     pointer-events: none;
     z-index: 9;
+    display: none;
 
     label {
       color: #252525;
@@ -19,6 +20,10 @@ export const Cursor = styled.div`
       font-size: 1.8rem;
       font-weight: 400;
     }
+
+    ${media.greaterThan('medium')`
+      display: flex;
+    `}
   `}
 `
 
