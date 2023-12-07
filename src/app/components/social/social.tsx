a18e44730cd31b58b2dfe4c76a0f91ff33a61bc7
AndreQuintero <andre.quintero96@gmail.com>
2023-12-07 23:40:42 +0800

    add: linkedin link

--- src/app/components/social/social.tsx
+++ src/app/components/social/social.tsx
@@ -4,7 +4,12 @@ import * as S from './style'
 export const Social = () => {
   return (
     <S.Wrapper>
-      <Button colorScheme="primary" icon="linkedin">
+      <Button
+        colorScheme="primary"
+        icon="linkedin"
+        as="a"
+        href="https://www.linkedin.com/in/gabrielchelles/"
+        target="_blank">
         /gabrielChelles
       </Button>
       <Button colorScheme="primary" icon="download">
