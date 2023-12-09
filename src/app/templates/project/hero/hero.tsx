e84c36226a5f2e05962e327b24807d47ffb9a2df
AndreQuintero <andre.quintero96@gmail.com>
2023-12-09 20:41:10 +0800

    feature: hero section project detail implementation

---
+++ src/app/templates/project/hero/hero.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { Container } from '@/app/components/container'
+import * as S from './style'
+import { fadeIn } from '@/app/motion'
+
+export const Hero = () => {
+  return (
+    <S.Wrapper>
+      <Container>
+        <S.Title
+          variants={fadeIn(0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}>
+          Transformando a experiência de compras no Shopping Iguatemi Fortaleza
+        </S.Title>
+        <S.Subtitle
+          variants={fadeIn(0.4)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}>
+          Construindo um Marketplace inovador em sintonia com o “Novo Normal”
+          durante/pós pandemia e elevando o padrão de atendimento ao cliente.
+        </S.Subtitle>
+        <S.VisitWrapper
+          variants={fadeIn(0.6)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}>
+          <S.Text>Acesse o site</S.Text>
+          <S.Link>https://www.iguatemifortaleza.com.br/</S.Link>
+        </S.VisitWrapper>
+      </Container>
+    </S.Wrapper>
+  )
+}
---
+++ src/app/templates/project/hero/style.ts
@@ -0,0 +1,70 @@
+import { motion } from 'framer-motion'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.section`
+  padding: 6.4rem 0;
+
+  ${media.greaterThan('medium')`
+        padding: 10.4rem 0;
+    `}
+`
+
+export const Title = styled(motion.h1)`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.style.primary};
+    font-size: 2.8rem;
+    font-weight: 400;
+    ${media.greaterThan('medium')`
+        font-size: 7.2rem;
+    `}
+  `}
+`
+
+export const Subtitle = styled(motion.h4)`
+  ${({ theme }) => css`
+    margin-top: 2.4rem;
+    font-family: ${theme.fonts.style.secondary};
+    font-size: 1.4rem;
+    font-weight: 400;
+    ${media.greaterThan('medium')`
+        margin-top: 4.8rem;
+        font-size: 2.4rem;
+    `}
+  `}
+`
+
+export const VisitWrapper = styled(motion.div)`
+  margin-top: 2.4rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  ${media.greaterThan('medium')`
+    margin-top: 4.8rem;
+    gap: 0.8rem;
+  `}
+`
+
+export const Text = styled.h4`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.style.primary};
+    font-size: 1.4rem;
+    font-weight: 400;
+    ${media.greaterThan('medium')`
+        font-size: 2.4rem;
+    `}
+  `}
+`
+
+export const Link = styled.a`
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.style.secondary};
+    font-size: 1.4rem;
+    font-weight: 400;
+    color: ${theme.colors.secondary};
+    text-decoration-line: underline;
+    ${media.greaterThan('medium')`
+        font-size: 2.4rem;
+    `}
+  `}
+`
