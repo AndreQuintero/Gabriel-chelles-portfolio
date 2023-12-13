593ba507919dd267a490898d6d20dfa402bffac5
AndreQuintero <andre.quintero96@gmail.com>
2023-12-14 00:38:32 +0800

    change: using list component

--- src/app/templates/project/goals/goals.tsx
+++ src/app/templates/project/goals/goals.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Container } from '@/app/components/container'
 import * as S from './style'
+import { List } from '@/app/components/list'
 
 export const Goals = () => {
   return (
@@ -8,29 +9,26 @@ export const Goals = () => {
       <S.Wraper>
         <S.Details $position="left">
           <S.Title>Objetivos</S.Title>
-          <S.List>
-            <S.Item>Criar um marketplace.</S.Item>
-            <S.Item>Preservar a experiência física.</S.Item>
-            <S.Item>
-              Entender novos comportamentos de compra durante a pandemia.
-            </S.Item>
-            <S.Item>Identificar novas possibilidades para o negócio.</S.Item>
-          </S.List>
+          <List
+            space="small"
+            items={[
+              'Criar um marketplace.',
+              'Preservar a experiência física.',
+              'Entender novos comportamentos de compra durante a pandemia.',
+              'Identificar novas possibilidades para o negócio.'
+            ]}
+          />
         </S.Details>
         <S.Details $position="right">
           <S.Title>Resultados</S.Title>
-          <S.List>
-            <S.Item>
-              Marketplace integrado com uma experiência física do shopping.
-            </S.Item>
-            <S.Item>
-              Mais de 60 lojistas participando no dia que foi anunciado.
-            </S.Item>
-            <S.Item>
-              Entendimento sobre o público do Guatemi e seu comportamento de
-              consumo durante a pandemia.
-            </S.Item>
-          </S.List>
+          <List
+            space="small"
+            items={[
+              'Marketplace integrado com uma experiência física do shopping.',
+              'Mais de 60 lojistas participando no dia que foi anunciado.',
+              'Entendimento sobre o público do Guatemi e seu comportamento de consumo durante a pandemia.'
+            ]}
+          />
         </S.Details>
       </S.Wraper>
     </Container>
--- src/app/templates/project/goals/style.ts
+++ src/app/templates/project/goals/style.ts
@@ -42,22 +42,3 @@ export const Title = styled.h4`
     `}
   `}
 `
-
-export const List = styled.ul`
-  padding-left: 2.2rem;
-`
-
-export const Item = styled.li`
-  ${({ theme }) => css`
-    font-family: ${theme.fonts.style.secondary};
-    font-size: 1.4rem;
-    font-weight: 400;
-    margin-top: 0.8rem;
-    &:first-child {
-      margin-top: 0;
-    }
-    ${media.greaterThan('medium')`
-        font-size: 1.6rem;
-    `}
-  `}
-`
