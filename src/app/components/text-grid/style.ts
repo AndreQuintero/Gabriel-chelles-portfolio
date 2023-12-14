0387998801a24541ab051d4956e0383807b38812
AndreQuintero <andre.quintero96@gmail.com>
2023-12-15 01:01:00 +0800

    change: gap property to grid-column-gap

--- src/app/components/text-grid/style.ts
+++ src/app/components/text-grid/style.ts
@@ -4,9 +4,9 @@ import media from 'styled-media-query'
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  gap: 2.4rem;
+  grid-column-gap: 2.4rem;
   ${media.greaterThan('medium')`
         grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-        gap: 3rem;
+        grid-column-gap: 3rem;
     `}
 `
