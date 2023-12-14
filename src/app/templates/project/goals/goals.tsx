b2127e3417e05da31ee32cf21e1e0eeabbd83748
AndreQuintero <andre.quintero96@gmail.com>
2023-12-15 00:43:47 +0800

    add: using text grid component

--- src/app/templates/project/goals/goals.tsx
+++ src/app/templates/project/goals/goals.tsx
@@ -2,34 +2,37 @@
 import { Container } from '@/app/components/container'
 import * as S from './style'
 import { List } from '@/app/components/list'
+import { TextGrid } from '@/app/components/text-grid'
 
 export const Goals = () => {
   return (
     <Container>
       <S.Wraper>
-        <S.Details $position="left">
-          <S.Title>Objetivos</S.Title>
-          <List
-            space="small"
-            items={[
-              'Criar um marketplace.',
-              'Preservar a experiência física.',
-              'Entender novos comportamentos de compra durante a pandemia.',
-              'Identificar novas possibilidades para o negócio.'
-            ]}
-          />
-        </S.Details>
-        <S.Details $position="right">
-          <S.Title>Resultados</S.Title>
-          <List
-            space="small"
-            items={[
-              'Marketplace integrado com uma experiência física do shopping.',
-              'Mais de 60 lojistas participando no dia que foi anunciado.',
-              'Entendimento sobre o público do Guatemi e seu comportamento de consumo durante a pandemia.'
-            ]}
-          />
-        </S.Details>
+        <TextGrid>
+          <S.Details $position="left">
+            <S.Title>Objetivos</S.Title>
+            <List
+              space="small"
+              items={[
+                'Criar um marketplace.',
+                'Preservar a experiência física.',
+                'Entender novos comportamentos de compra durante a pandemia.',
+                'Identificar novas possibilidades para o negócio.'
+              ]}
+            />
+          </S.Details>
+          <S.Details $position="right">
+            <S.Title>Resultados</S.Title>
+            <List
+              space="small"
+              items={[
+                'Marketplace integrado com uma experiência física do shopping.',
+                'Mais de 60 lojistas participando no dia que foi anunciado.',
+                'Entendimento sobre o público do Guatemi e seu comportamento de consumo durante a pandemia.'
+              ]}
+            />
+          </S.Details>
+        </TextGrid>
       </S.Wraper>
     </Container>
   )
--- src/app/templates/project/summary/summary.tsx
+++ src/app/templates/project/summary/summary.tsx
@@ -1,30 +1,34 @@
 'use client'
 import { Container } from '@/app/components/container'
 import * as S from './style'
+import { TextGrid } from '@/app/components/text-grid'
 
 export const Summary = () => {
   return (
     <Container>
       <S.Wrapper>
-        <S.Description>
-          Inaugurado em 1982, o Iguatemi Fortaleza é um dos maiores shoppings do
-          Ceará e é referência em inovação na região. Com mais de 450 lojas, o
-          shopping sofreu com o impacto de uma pandemia e buscou se digitalizar
-          para conseguir se manter de pé nesse novo cenário mundial.
-        </S.Description>
-        <S.DetailsWrapper>
-          <S.Details>
-            <S.Title>Minha função</S.Title>
-            <S.Information>
-              UX Designer com foco em pesquisas e definição estratégica do
-              produto.
-            </S.Information>
-          </S.Details>
-          <S.Details>
-            <S.Title>Ano do projeto</S.Title>
-            <S.Information>2020/2021</S.Information>
-          </S.Details>
-        </S.DetailsWrapper>
+        <TextGrid>
+          <S.Description>
+            Inaugurado em 1982, o Iguatemi Fortaleza é um dos maiores shoppings
+            do Ceará e é referência em inovação na região. Com mais de 450
+            lojas, o shopping sofreu com o impacto de uma pandemia e buscou se
+            digitalizar para conseguir se manter de pé nesse novo cenário
+            mundial.
+          </S.Description>
+          <S.DetailsWrapper>
+            <S.Details>
+              <S.Title>Minha função</S.Title>
+              <S.Information>
+                UX Designer com foco em pesquisas e definição estratégica do
+                produto.
+              </S.Information>
+            </S.Details>
+            <S.Details>
+              <S.Title>Ano do projeto</S.Title>
+              <S.Information>2020/2021</S.Information>
+            </S.Details>
+          </S.DetailsWrapper>
+        </TextGrid>
       </S.Wrapper>
     </Container>
   )
