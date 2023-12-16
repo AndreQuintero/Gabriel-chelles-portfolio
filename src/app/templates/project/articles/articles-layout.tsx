c1450c35e7e8ccb2e2a8ff48c16ab233df3bccf0
AndreQuintero <andre.quintero96@gmail.com>
2023-12-16 23:16:39 +0800

    feature: articles layout implementation

---
+++ src/app/templates/project/articles/articles-layout.tsx
@@ -0,0 +1,26 @@
+import { Article, MultiArticles } from '@/app/components/article'
+import { ArticleSection } from './articles'
+import { useViewDetect } from '@/app/hooks/useViewDetect'
+
+type SingleArticlesProps = Pick<ArticleSection, 'articles'>
+export const SingleArticlesSection = ({ articles }: SingleArticlesProps) => {
+  return (
+    <>
+      {articles.map((article, index) => (
+        <Article
+          key={`single-article-${article.title}-${index}`}
+          {...article}
+        />
+      ))}
+    </>
+  )
+}
+
+export const MultiArticlesSection = ({ articles }: SingleArticlesProps) => {
+  const { isMobile } = useViewDetect()
+  return isMobile ? (
+    <SingleArticlesSection articles={articles} />
+  ) : (
+    <MultiArticles articles={articles} />
+  )
+}
