17c2ee95636e1f203ac34cb7e61d0a09cd721d55
AndreQuintero <andre.quintero96@gmail.com>
2023-12-19 01:45:27 +0800

    add: scroll top when change routes

--- src/app/components/wrapper/wrapper.tsx
+++ src/app/components/wrapper/wrapper.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { ReactNode, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { darkTheme, lightTheme, Theme } from '@/app/style/theme'
 import { useTheme } from '@/app/providers/ThemeProvider'
+import { useRouter } from 'next/navigation'
 
 type Props = {
   children: ReactNode
@@ -10,6 +11,14 @@ type Props = {
 
 export const Wrapper = ({ children }: Props) => {
   const { theme } = useTheme()
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      // Scroll to the top of the page when the route changes
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+  }, [router])
 
   const themetype = theme === Theme.LIGHT ? lightTheme : darkTheme
   return <ThemeProvider theme={themetype}>{children}</ThemeProvider>
