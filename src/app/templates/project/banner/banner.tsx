d3b026af57d72a46880380639f7aad0c634e13ca
AndreQuintero <andre.quintero96@gmail.com>
2023-12-19 01:23:38 +0800

    change: link image

--- src/app/templates/project/banner/banner.tsx
+++ src/app/templates/project/banner/banner.tsx
@@ -2,7 +2,7 @@
 import * as S from './style'
 
 const url =
-  'https://s3-alpha-sig.figma.com/img/b5b0/5c6f/2a8a06a61daff9ffdff47096cfd7068c?Expires=1702857600&Signature=Rv-a3IEDcMge~EBU88ASNybj5wsa5KMlBIUxiDpMmKGUWWPb3uIP450bGVF9B19nNxgoLra95aAgpiH0WhiKHdDQyaWmlEElAWEOcv7SFLvbmEzSbZvvDbxUkKEPN~wv7eQExWmIExHBkBfYAXzLIai4JbBzk49tSFJDYuDHOKofaJBCNK4REGpVgOdT-HWLejT3BMO88xhVAyVT8WPhCCnc7uIt2e~Ig6xRhmcHj4y506eglfdpnEezAdnklyYjjm~gAYUsc3~fHKnr6lqhF-iVNuSl-KCilELS83x8qLmRs~fTQerhOAjgFFu90IzLXZVl-x7oc06~IjhAA857tA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+  'https://s3-alpha-sig.figma.com/img/b5b0/5c6f/2a8a06a61daff9ffdff47096cfd7068c?Expires=1704067200&Signature=RepvMADzWwMedkpnQlr1h-50pKxQxluZzv2oAXymrtepfT-MVdpAEdse3kTGt90wtVM0RwixkAPwd1CZaeOVaK~xnzfb7z7~HrcLhfnZQuxhGnsQ9nyKeodBQGo~-YzoOOsNi0gU2aJCVcosXF-DHuLDVMs5MUkdnsnX~KEIHV05RgsG95ahSnvYLsUdRBfZ~AgSBN94nCMm4y66D7nh0VrS-q94La4Xx5O7hRpvs9oZWQsXVKHuU9Et3BNA~kcQcT6Uzu7ctZtAAn8zKxZ1ikUIdEG5Vgrj1FfQrH98abMBA5Jrk83VZavQ6IwoHqHjP65DGP6Z0ciHVcQLMb8CWg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
 export const Banner = () => {
   return <S.Banner $url={url} />
 }
