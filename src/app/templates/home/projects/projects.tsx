02f52d0f0f616ba42b7021033d72db30aa79b675
AndreQuintero <andre.quintero96@gmail.com>
2023-12-27 00:24:44 +0800

    add: route to projects

--- src/app/templates/home/projects/projects.tsx
+++ src/app/templates/home/projects/projects.tsx
@@ -6,22 +6,26 @@ import * as S from './style'
 import { Button } from '@/app/components/button'
 import { ProjectsList } from '@/app/components/projects-list'
 import { ProjectCardProps } from '@/app/components/project-card'
-import { Link } from '@/app/components/link'
 import { Routes } from '@/app/routes'
+import { useRouter } from 'next/navigation'
 
 export type ProjectProps = {
   projects: Array<ProjectCardProps>
 }
 
 export const Projects = ({ projects }: ProjectProps) => {
+  const router = useRouter()
   return (
     <S.Wrapper>
       <Container>
         <ProjectsList projects={projects} />
         <S.MoreProjects>
-          <Link href={Routes.PROJECTS}>
-            <Button colorScheme="secondary">Ver todos os 12 projetos</Button>
-          </Link>
+          <Button
+            as="a"
+            colorScheme="secondary"
+            onClick={() => router.push(Routes.PROJECTS)}>
+            Ver todos os 12 projetos
+          </Button>
         </S.MoreProjects>
       </Container>
     </S.Wrapper>
