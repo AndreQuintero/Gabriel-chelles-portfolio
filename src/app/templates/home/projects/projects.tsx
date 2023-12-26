2c77669a80e6960cba523927b09d05897004ce52
AndreQuintero <andre.quintero96@gmail.com>
2023-12-27 00:06:31 +0800

    add: link to projects page

--- src/app/templates/home/projects/projects.tsx
+++ src/app/templates/home/projects/projects.tsx
@@ -6,6 +6,8 @@ import * as S from './style'
 import { Button } from '@/app/components/button'
 import { ProjectsList } from '@/app/components/projects-list'
 import { ProjectCardProps } from '@/app/components/project-card'
+import { Link } from '@/app/components/link'
+import { Routes } from '@/app/routes'
 
 export type ProjectProps = {
   projects: Array<ProjectCardProps>
@@ -17,7 +19,9 @@ export const Projects = ({ projects }: ProjectProps) => {
       <Container>
         <ProjectsList projects={projects} />
         <S.MoreProjects>
-          <Button colorScheme="secondary">Ver todos os 12 projetos</Button>
+          <Link href={Routes.PROJECTS}>
+            <Button colorScheme="secondary">Ver todos os 12 projetos</Button>
+          </Link>
         </S.MoreProjects>
       </Container>
     </S.Wrapper>
