c0e09d7fcca4414fa869c24232038cf70c0a51da
AndreQuintero <andre.quintero96@gmail.com>
2023-12-26 23:13:38 +0800

    add: setting the light theme every time that the route is changed

--- src/app/components/change-theme/change-theme.tsx
+++ src/app/components/change-theme/change-theme.tsx
@@ -1,14 +1,20 @@
-import { ReactNode } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { ReactNode, useEffect } from 'react'
 import { ObserverComponentVisibility } from '../observer-component-visibility'
 import { Theme } from '@/app/style/theme'
 import { useTheme } from '@/app/providers/ThemeProvider'
+import { usePathname } from 'next/navigation'
 
 type ChangeThemeProps = {
   children: ReactNode
 }
 export const ChangeTheme = ({ children }: ChangeThemeProps) => {
   const { setTheme } = useTheme()
+  const pathname = usePathname()
 
+  useEffect(() => {
+    setTheme(Theme.LIGHT)
+  }, [pathname])
   return (
     <ObserverComponentVisibility
       onComponentVisible={() => setTheme(Theme.DARK)}
