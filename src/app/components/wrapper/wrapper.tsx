7631378718a003ebefbe0be4aed0834bf4593e34
AndreQuintero <andre.quintero96@gmail.com>
2023-12-26 23:14:40 +0800

    change: removing router implementation

--- src/app/components/wrapper/wrapper.tsx
+++ src/app/components/wrapper/wrapper.tsx
@@ -1,9 +1,8 @@
 'use client'
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { darkTheme, lightTheme, Theme } from '@/app/style/theme'
 import { useTheme } from '@/app/providers/ThemeProvider'
-import { useRouter } from 'next/navigation'
 
 type Props = {
   children: ReactNode
@@ -11,14 +10,6 @@ type Props = {
 
 export const Wrapper = ({ children }: Props) => {
   const { theme } = useTheme()
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      // Scroll to the top of the page when the route changes
-      window.scrollTo({ top: 0, behavior: 'smooth' })
-    }
-  }, [router])
 
   const themetype = theme === Theme.LIGHT ? lightTheme : darkTheme
   return <ThemeProvider theme={themetype}>{children}</ThemeProvider>
