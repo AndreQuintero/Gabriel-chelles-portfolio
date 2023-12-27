c0daa3827cce95951d1ddafddc9975e29de4be0a
AndreQuintero <andre.quintero96@gmail.com>
2023-12-27 10:53:54 +0800

    add: title

--- src/app/templates/projects/projects.tsx
+++ src/app/templates/projects/projects.tsx
@@ -3,11 +3,19 @@ import { Container } from '@/app/components/container'
 import * as S from './style'
 import { ProjectsList } from '@/app/components/projects-list'
 import { ProjectProps } from '../home/projects/projects'
+import { fadeIn } from '@/app/motion'
 
 export const Projects = ({ projects }: ProjectProps) => {
   return (
     <S.Wrapper>
       <Container>
+        <S.Title
+          variants={fadeIn(0.15)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}>
+          Projetos
+        </S.Title>
         <ProjectsList projects={projects} />
       </Container>
     </S.Wrapper>
--- src/app/templates/projects/style.ts
+++ src/app/templates/projects/style.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import { motion } from 'framer-motion'
+import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
 export const Wrapper = styled.section`
@@ -8,4 +9,17 @@ export const Wrapper = styled.section`
         margin-top: 12.8rem;
         padding-bottom: 12.8rem;
     `}
+`
+export const Title = styled(motion.h1)`
+  ${({ theme }) => css`
+    font-family: ${theme.colors.secondary};
+    font-size: 2.8rem;
+    font-style: normal;
+    font-weight: 400;
+    margin-bottom: 6.4rem;
+    ${media.greaterThan('medium')`
+      font-size: 7.2rem;
+      margin-bottom: 12.8rem;
+    `}
+  `}
 `
