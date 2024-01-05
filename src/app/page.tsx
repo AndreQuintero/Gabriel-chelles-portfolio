c2cdfe5e04e8998d2517fa57839fa79aa573b78e
AndreQuintero <andre.quintero96@gmail.com>
2024-01-05 12:15:59 +0800

    change: using convert data function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { ImageProps, ProjectCardProps } from './components/project-card'
 import { hygraph } from './graphql/client'
-import { HOME, PROJECTS_FIRST_FOUR } from './graphql/queries'
-import { HomeProps, ProjectsProps } from './graphql/types'
+import { HOME } from './graphql/queries'
+import { HomeData, HomeProps } from './graphql/types'
+import { convertProjectDataToProjectCardProps } from './services/project'
 import { Hero } from './templates/home/hero'
 import { Projects } from './templates/home/projects'
 import { Video } from './templates/home/video'
 
-export const revalidate = 3600
+export const revalidate = process.env.REVALIDATE_TIME
 
 const getHomeData = async () => {
   const data: HomeProps = await hygraph.request(HOME)
@@ -14,27 +15,13 @@ const getHomeData = async () => {
   return data.homes[0]
 }
 
-const getProjects = async () => {
-  const data: ProjectsProps = await hygraph.request(PROJECTS_FIRST_FOUR)
-  const projects: ProjectCardProps[] = data?.projects?.map((project) => {
-    const images: ImageProps[] = project?.imagesThumb?.map((image) => {
-      return {
-        url: image?.imagem?.url,
-        description: image.description
-      }
-    })
-    return {
-      slug: project.slug,
-      title: project.title,
-      images
-    }
-  })
-  return projects
+const getProjects = (data: HomeData) => {
+  return convertProjectDataToProjectCardProps(data.projects)
 }
 
 export default async function Home() {
   const homeData = await getHomeData()
-  const projectData = await getProjects()
+  const projectData = getProjects(homeData!)
   const totalProjects = projectData.length
   return (
     <main>
