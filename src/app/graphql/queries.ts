f091321b8cbf913ae9022aba144b9a4d343d2424
AndreQuintero <andre.quintero96@gmail.com>
2024-01-05 23:39:46 +0800

    add: case query

--- src/app/graphql/queries.ts
+++ src/app/graphql/queries.ts
@@ -48,3 +48,71 @@ export const PROJECTS = gql`
     }
   }
 `
+
+export const CASE = gql`
+  query CaseByProjectSlug($slug: String!) {
+    cases(first: 1, where: { project: { slug: $slug } }) {
+      id
+      title
+      subtitle
+      linkTitle
+      link {
+        link
+        text
+      }
+      banner {
+        description
+        imagem {
+          url
+        }
+      }
+      description
+      myRole
+      projectYear
+      goals
+      results
+      sectionArticles {
+        sectionType
+        articles {
+          title
+          introduction
+          items
+          conclusion
+          images {
+            fullWidth
+            image {
+              description
+              imagem {
+                url
+              }
+            }
+          }
+          showcases {
+            image {
+              description
+              imagem {
+                url
+              }
+            }
+          }
+        }
+      }
+      nextPage {
+        project {
+          slug
+          title
+        }
+        color {
+          hex
+        }
+        textColor
+        image {
+          description
+          imagem {
+            url
+          }
+        }
+      }
+    }
+  }
+`
