43f25702874e127d0b80918a83cc0c6de89cbb25
AndreQuintero <andre.quintero96@gmail.com>
2024-01-07 01:04:34 +0800

    add: metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,45 @@ export const outfit = Outfit({
 
 export const metadata: Metadata = {
   title: 'Gabriel Chelles Portfolio',
-  description: 'Gabriel Chelles UX/UI designer'
+  description: 'Gabriel Chelles UX/UI designer',
+  applicationName: 'Gabriel Chelles Portfolio',
+  abstract: 'Portfolio de Gabriel Chelles contendo todos os seus projetos',
+  appleWebApp: {
+    title: 'Gabriel Chelles Portfolio',
+    statusBarStyle: 'black-translucent'
+  },
+  authors: {
+    name: 'Gabriel Chelles'
+  },
+  category: 'Portfolio',
+  creator: 'Andre Quintero dos Santos',
+  generator: 'Next.js',
+  keywords: [
+    'Gabriel',
+    'Chelles',
+    'gabriel',
+    'chelles',
+    'site',
+    'website',
+    'projetos',
+    'portfolio',
+    'UX',
+    'UI',
+    'design',
+    'figma',
+    'blog',
+    'react',
+    'next js',
+    'vercel'
+  ],
+  publisher: 'Vercel',
+  openGraph: {
+    type: 'website',
+    description: 'Portfolio de Gabriel Chelles contendo todos os seus projetos',
+    siteName: 'Gabriel Chelles Portfolio',
+    title: 'Gabriel Chelles',
+    url: process.env.DOMAIN
+  }
 }
 
 export const viewport: Viewport = {
