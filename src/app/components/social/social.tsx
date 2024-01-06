cbc1b481bd18ade12307cf9f3b4ec9b781610803
AndreQuintero <andre.quintero96@gmail.com>
2024-01-06 21:58:56 +0800

    add: contact social props

--- src/app/components/social/social.tsx
+++ src/app/components/social/social.tsx
@@ -1,20 +1,34 @@
+import { ContactProps, LinkData } from '@/app/graphql/types'
 import { Button } from '../button'
 import * as S from './style'
 
-export const Social = () => {
+export const Social = ({
+  resumeText,
+  resumeDownloadText,
+  resume,
+  linkedin
+}: ContactProps) => {
   return (
     <S.Wrapper>
       <Button
         colorScheme="primary"
         icon="linkedin"
         as="a"
-        href="https://www.linkedin.com/in/gabrielchelles/"
-        target="_blank">
-        /gabrielChelles
+        href={linkedin.link}
+        target="_blank"
+        rel="noopener noreferrer">
+        {linkedin.text}
       </Button>
-      <Button colorScheme="primary" icon="download">
-        Download CV
-      </Button>
+      {resume && (
+        <Button
+          as="a"
+          href={`/api/pdf?download=${resume.url}`}
+          download={`${resumeDownloadText}.pdf`}
+          colorScheme="primary"
+          icon="download">
+          {resumeText}
+        </Button>
+      )}
     </S.Wrapper>
   )
 }
