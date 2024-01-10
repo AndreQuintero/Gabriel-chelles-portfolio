0a0ed172d9d69e0fff415120dfbc3138c3e384e6
AndreQuintero <andre.quintero96@gmail.com>
2024-01-11 01:22:23 +0800

    add: background color

--- src/app/templates/project/banner/style.ts
+++ src/app/templates/project/banner/style.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
 export const Banner = styled.section<{ $url: string }>`
-  ${({ $url }) => css`
+  ${({ theme, $url }) => css`
     width: 100%;
     height: 33.5rem;
     background-image: url(${$url});
@@ -10,6 +10,7 @@ export const Banner = styled.section<{ $url: string }>`
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
+    background-color: ${theme.colors.tertiary};
     ${media.greaterThan('medium')`
         height: 72rem;
         background-attachment: fixed;
