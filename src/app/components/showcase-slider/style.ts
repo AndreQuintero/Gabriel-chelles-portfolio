384b47d2a019d1b031233283c3fa745e060ea182
AndreQuintero <andre.quintero96@gmail.com>
2024-01-12 02:16:11 +0800

    change: card position

--- src/app/components/showcase-slider/style.ts
+++ src/app/components/showcase-slider/style.ts
@@ -5,7 +5,7 @@ export const ShowcaseSlider = styled.div`
   margin-top: 1.6rem;
   .slick-track {
     display: flex;
-    margin: -10px -10px; /* Adjust the negative margin to control the gap size for rows and columns */
+    margin: -10px 13%; /* Adjust the negative margin to control the gap size for rows and columns */
   }
 
   .slick-slide {
