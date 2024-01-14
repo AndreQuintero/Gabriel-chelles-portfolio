0b2fbad4613cc030120413fd2fc3cbfd6fdc8064
AndreQuintero <andre.quintero96@gmail.com>
2024-01-14 22:16:53 +0800

    change: remove metatags static

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from 'next'
+import type { Viewport } from 'next'
 import { Poppins, Outfit } from 'next/font/google'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
@@ -15,6 +15,7 @@ import { FooterProps } from './graphql/types'
 import { Hotjar } from './components/hotjar'
 import { GoogleAnalytics } from './components/google-analytics'
 import { MenuProvider } from './providers/MenuProvider'
+import { getMetaDataApi } from './services/metadata'
 
 export const poppins = Poppins({
   subsets: ['latin'],
@@ -30,105 +31,6 @@ export const outfit = Outfit({
   variable: '--font-outfit'
 })
 
-export const metadata: Metadata = {
-  metadataBase: new URL(process.env.DOMAIN!),
-  title: 'Gabriel Chelles Product Designer',
-  description: 'Portfolio de Gabriel Chelles contendo todos os seus projetos',
-  applicationName: 'Gabriel Chelles Product Designer',
-  abstract: 'Gabriel Chelles Product|UX|UI designer',
-  icons: {
-    icon: {
-      url: '/images/favicon.png',
-      href: '/images/favicon.png'
-    }
-  },
-  appleWebApp: {
-    title: 'Gabriel Chelles Product Designer',
-    statusBarStyle: 'black-translucent',
-    startupImage: {
-      url: '/images/favicon.png'
-    }
-  },
-  authors: {
-    name: 'Gabriel Chelles'
-  },
-  category: 'Portfolio',
-  creator: 'Andre Quintero dos Santos',
-  generator: 'Next.js',
-  keywords: [
-    'Gabriel',
-    'Chelles',
-    'gabriel',
-    'chelles',
-    'site',
-    'website',
-    'projetos',
-    'portfolio',
-    'UX',
-    'UI',
-    'design',
-    'figma',
-    'blog',
-    'photoshop',
-    'illustrator',
-    'after effects',
-    'premiere',
-    'hotjar',
-    'analytics',
-    'responsive web design',
-    'teste de usabilidade',
-    'usabilty test',
-    'product discovery',
-    'qualitative research',
-    'pesquisa qualitativa',
-    'quantitative research',
-    'pesquisa quantitativa',
-    'interface',
-    'product delivery',
-    'handoff',
-    'typography',
-    'tipografia',
-    'product strategy',
-    'estratégia',
-    'user flows',
-    'user stories',
-    'visual design',
-    'prototyping',
-    'protótipo',
-    'design system',
-    'user research',
-    'pesquisa com usuário',
-    'mobile',
-    'design sprint',
-    'interaction design',
-    'design thinking',
-    'user interview',
-    'information architecture',
-    'customer journey map',
-    'heuristics analysis',
-    'wireframing',
-    'workshop facilitation',
-    'data informed design'
-  ],
-  publisher: 'Vercel',
-  openGraph: {
-    type: 'website',
-    description: 'Portfolio de Gabriel Chelles contendo todos os seus projetos',
-    siteName: 'Gabriel Chelles Product Designer',
-    title: 'Gabriel Chelles',
-    url: process.env.DOMAIN,
-    images: [
-      {
-        url: 'https://media.graphassets.com/hn7hLJGGS5G3DAI9cauw'
-      }
-    ]
-  }
-}
-
-export const viewport: Viewport = {
-  themeColor: '#000'
-}
-
 export const revalidate = process.env.REVALIDATE_TIME
 
 const getFooter = async () => {
@@ -137,16 +39,25 @@ const getFooter = async () => {
   return data.footers[0]
 }
 
+export const viewport: Viewport = {
+  themeColor: '#000'
+}
 export default async function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
   const data = await getFooter()
+  const metatag = await getMetaDataApi()
   return (
     <html lang="pt-br">
       <head>
-        <link rel="shortcut icon" href="/images/favicon.png" />
+        {!!metatag && !!metatag.maskIcon && (
+          <link
+            rel="mask-icon"
+            href={metatag.maskIcon.url}
+            color={metatag.maskIconColor}></link>
+        )}
         <GoogleAnalytics />
         <Hotjar />
       </head>
